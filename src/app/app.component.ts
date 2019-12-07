d1b34c3f55b8ae15fc57ce57b935b3af030b0070
Lance Finney <lance.finney@oasisdigital.com>
2019-12-06 21:54:33 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
   // ,providers: [CarStateService]
 })
 export class AppComponent {
